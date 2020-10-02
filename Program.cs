76e8e071a1fab737bb20774b2513257bebe0ba97
Vishal Garg <vishugarg10004@gmail.com>
2020-10-02 16:02:31 +0530

    [Vishal] Added contact details manually, made a list, added nLog file

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Management System");
+            ContactPersonInformation contactPersonInformation = new ContactPersonInformation();
+            contactPersonInformation.AddingContactDetails("Vishal", "Garg", "a", "b", "c", 1, 2, "vishal.garg");
+            contactPersonInformation.AddingContactDetails("Lakshay", "Garg", "a", "b", "d", 3,5, "lakshay.garg");
+            contactPersonInformation.DisplayContactDetails();
         }
     }
 }
